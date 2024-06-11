60f941b6ef5fa46d499fe993a79b809e81b3ee8f
naufalamr17 <naufal.amr17@outlook.co.id>
2024-06-11 12:17:42 +0700

    Update: Change footer

--- resources/views/components/footers/auth.blade.php
+++ resources/views/components/footers/auth.blade.php
@@ -5,15 +5,11 @@
                 <div class="copyright text-center text-sm text-muted text-lg-start">
                     © <script>
                         document.write(new Date().getFullYear())
-
                     </script>,
-                    made with <i class="fa fa-heart"></i> by
-                    <a href="https://www.creative-tim.com" class="font-weight-bold" target="_blank">Creative Tim</a> &
-                    <a href="https://www.updivision.com" class="font-weight-bold" target="_blank"> UPDIVISION</a>
-                    for a better web.
+                    <a href="#" class="font-weight-bold">PT. Makmur Lestari Primatama IT Department</a>
                 </div>
             </div>
-            <div class="col-lg-6">
+            <!-- <div class="col-lg-6">
                 <ul class="nav nav-footer justify-content-center justify-content-lg-end">
                     <li class="nav-item">
                         <a href="https://www.creative-tim.com" class="nav-link text-muted" target="_blank">Creative
@@ -34,7 +30,7 @@
                             target="_blank">License</a>
                     </li>
                 </ul>
-            </div>
+            </div> -->
         </div>
     </div>
 </footer>

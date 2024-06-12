c273cbd311ba9bffd2264a1e2ad7bb73b517f69a
naufalamr17 <naufal.amr17@outlook.co.id>
2024-06-12 21:41:37 +0700

    Update: Use laravel excel

--- resources/views/components/navbars/sidebar.blade.php
+++ resources/views/components/navbars/sidebar.blade.php
@@ -71,7 +71,7 @@
     </div>
     <div class="sidenav-footer position-absolute w-100 bottom-0 ">
         <div class="mx-3">
-            <a class="btn bg-gradient-danger w-100" href="https://www.creative-tim.com/product/material-dashboard-pro-laravel" type="button">Import Data</a>
+            <a class="btn bg-gradient-danger w-100" href="" type="button">Import Data</a>
         </div>
     </div>
 </aside>

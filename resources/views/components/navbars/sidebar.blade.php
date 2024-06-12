15a061c2b5e471ee777739617a847be764bd6ae4
naufalamr17 <naufal.amr17@outlook.co.id>
2024-06-12 21:30:09 +0700

    Update: Add import button

--- resources/views/components/navbars/sidebar.blade.php
+++ resources/views/components/navbars/sidebar.blade.php
@@ -69,4 +69,9 @@
             </li>
         </ul>
     </div>
+    <div class="sidenav-footer position-absolute w-100 bottom-0 ">
+        <div class="mx-3">
+            <a class="btn bg-gradient-danger w-100" href="https://www.creative-tim.com/product/material-dashboard-pro-laravel" type="button">Import Data</a>
+        </div>
+    </div>
 </aside>

9b2c676e9717a1c6e6321658eb6d3378e76f1378
naufalamr17 <naufal.mtsyurja91@gmail.com>
2024-06-13 22:40:28 +0700

    Update: Add metadata

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -19,6 +19,7 @@
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
 
     <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('assets') }}/img/apple-icon.png">
     <link rel="icon" type="image/png" href="{{ asset('assets') }}/img/favicon.png">

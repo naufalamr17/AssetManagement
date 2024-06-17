ee3bda3d7f4f87087523c7b9bba51ec4632d5c6d
naufalamr17 <naufal.amr17@outlook.co.id>
2024-06-17 21:47:41 +0700

    Update: Fix dispose and repair table

--- resources/views/pages/asset/dispose.blade.php
+++ resources/views/pages/asset/dispose.blade.php
@@ -156,21 +156,25 @@
                                             <td>{{ $item->asset_type ?? '-' }}</td>
                                             <td>{{ $item->serial_number ?? '-' }}</td>
                                             <?php
-                                            $acquisitionDate = new DateTime($item->acquisition_date);
-                                            $usefulLife = $item->useful_life * 365; // Convert useful life from years to days
-                                            $endOfUsefulLife = clone $acquisitionDate;
-                                            $endOfUsefulLife->modify("+{$usefulLife} days");
-
-                                            $currentDate = new DateTime();
-                                            $interval = $currentDate->diff($endOfUsefulLife);
-
-                                            if ($currentDate > $endOfUsefulLife) {
-                                                $remainingDays = -$interval->days; // Use negative value for overdue days
+                                            if ($item->acquisition_date === '-') {
+                                                $message = "Tanggal tidak terdefinisi";
                                             } else {
-                                                $remainingDays = $interval->days;
-                                            }
+                                                $acquisitionDate = new DateTime($item->acquisition_date);
+                                                $usefulLife = $item->useful_life * 365; // Convert useful life from years to days
+                                                $endOfUsefulLife = clone $acquisitionDate;
+                                                $endOfUsefulLife->modify("+{$usefulLife} days");
 
-                                            $message = "{$remainingDays} hari";
+                                                $currentDate = new DateTime();
+                                                $interval = $currentDate->diff($endOfUsefulLife);
+
+                                                if ($currentDate > $endOfUsefulLife) {
+                                                    $remainingDays = -$interval->days; // Use negative value for overdue days
+                                                } else {
+                                                    $remainingDays = $interval->days;
+                                                }
+
+                                                $message = "{$remainingDays} hari";
+                                            }
                                             ?>
                                             <td>{{ $message }}</td>
                                             <td>{{ $item->location ?? '-' }}</td>

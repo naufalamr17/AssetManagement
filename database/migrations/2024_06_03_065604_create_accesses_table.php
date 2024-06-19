e8f90de9dcaa9a0ffc14b5d3ecc8f13c0c2a595c
naufalamr17 <naufal.amr17@outlook.co.id>
2024-06-19 11:32:24 +0700

    Update: add new status super admin

--- database/migrations/2024_06_03_065604_create_accesses_table.php
+++ database/migrations/2024_06_03_065604_create_accesses_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id(); // Menetapkan kolom 'id' sebagai kunci utama auto-increment
             $table->unsignedBigInteger('user_id'); // Menggunakan unsignedBigInteger untuk merujuk ke ID pengguna
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
-            $table->enum('access', ['Asset Management', 'IT Asset Management', 'ATK Management']); // ENUM access
+            $table->string('access'); // ENUM access
             $table->timestamps(); // Menambahkan kolom created_at dan updated_at
         });        
     }

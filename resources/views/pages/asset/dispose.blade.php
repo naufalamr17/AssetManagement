41b86db65b6d2016cbd1f3810e5be909bbfbdca5
naufalamr17 <naufal.amr17@outlook.co.id>
2024-11-13 15:01:21 +0700

    Update: Fix minor

--- resources/views/pages/asset/dispose.blade.php
+++ resources/views/pages/asset/dispose.blade.php
@@ -206,7 +206,7 @@
                                                 @if ($item->disposal_document)
                                                 <!-- Tampilkan tombol download jika disposal_document ada -->
                                                 <a href="{{ asset('storage/' . $item->disposal_document) }}" class="btn btn-sm mt-3 btn-secondary">DOWNLOAD DOKUMEN</a>
-                                                @elseif (strtoupper($item->approval) === 'APPROVE BY DEPUTY GENERAL MANAGER' || strtoupper($item->approval) === 'APPROVE BY MANAGER')
+                                                @elseif (strtoupper($item->approval) === 'APPROVE BY DEPUTY GENERAL MANAGER' || strtoupper($item->approval) === 'APPROVE BY MANAGER' || strtoupper($item->approval) === 'APPROVE BY SUPERVISOR')
                                                 <!-- Jika approval adalah 'Approve by Deputy General Manager', tampilkan tombol 'Add Document' -->
                                                 <a href="{{ route('add.document', $item->id) }}" class="btn btn-sm mt-3 btn-primary">ADD DOCUMENT</a>
                                                 @else
